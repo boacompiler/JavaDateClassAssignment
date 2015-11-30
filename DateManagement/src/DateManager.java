42f4b0ef8ea732d32734078ed36f12951e356735
Rob <20183128@U280.college.farnborough>
2015-11-30 13:26:03 +0000

    basic class setup constructor and getters setters

--- DateManagement/src/DateManager.java
+++ DateManagement/src/DateManager.java
@@ -1,4 +1,38 @@
 public class DateManager
 {
+	int year;
+	int month;
+	int day;
 
+	// constructor taking parameters in osi date standard order
+	public DateManager(int year, int month, int day)
+	{
+		this.year = year;
+		this.month = month;
+		this.day = day;
+	}
+	//getters and setters
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+    public int getYear() {
+       return year;
+    }
+    
+    public void setMonth(int month) {
+		this.month = month;
+	}
+
+    public int getMonth() {
+       return month;
+    }
+    
+    public void setDay(int day) {
+		this.day = day;
+	}
+
+    public int getDay() {
+       return day;
+    }
 }
