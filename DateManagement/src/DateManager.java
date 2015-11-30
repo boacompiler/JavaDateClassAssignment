1b8d514a001c481fd99f5a03f3ce0c3a5bbc0774
Rob <20183128@U070.college.farnborough>
2015-11-30 16:44:56 +0000

    added more functionality

--- DateManagement/src/DateManager.java
+++ DateManagement/src/DateManager.java
@@ -7,6 +7,11 @@ public class DateManager
 	// constructor taking parameters in osi date standard order
 	public DateManager(int year, int month, int day)
 	{
+		if(month > 12 || month < 1)
+		{
+			throw new IllegalArgumentException("Month must be between 1 and 12");
+		}
+		//TODO add day validation per month or use valid date, but this returns a bool so we cannot return a specialised error message
 		this.year = year;
 		this.month = month;
 		this.day = day;
@@ -46,13 +51,26 @@ public class DateManager
 	public boolean validDate(int year, int month, int day)
 	{
 		//TODO finish this
-		return true;
+		boolean valid = true;
+		if(month > 12 || month < 1)
+		{
+			valid = false;
+		}
+		
+		return valid;
 	}
 	
-	public String toString()
+	public String europeanString()
 	{
-		//TODO more formats
 		return day + "/" + month + "/"+ year;
+	}
+	public String americanString()
+	{
+		return month + "/" + day + "/"+ year;
+	}
+	public String internationalString()
+	{
+		return year + "/" + month + "/"+ day;
 	}
 	
 	public boolean leapYear()
@@ -64,7 +82,24 @@ public class DateManager
 	public int daysDifferent(DateManager initialDate,DateManager finalDate)
 	{
 		//TODO finish this
+		//http://mathforum.org/library/drmath/view/66857.html
+		daysFormulae(initialDate);
+		daysFormulae(finalDate);
 		return 0;
+	}
+	
+	private int daysFormulae(DateManager myDate)
+	{
+		if(myDate.month == 1 || myDate.month == 2)
+		{
+			myDate.setMonth(myDate.month + 12);
+			myDate.setYear(myDate.year - 1);
+		}
+		
+		//return 365*myDate.month + Math.floor(myDate.year/4) - Math.floor(myDate.year/100) + Math.floor(myDate.year/400) + myDate.day + Math.floor((153*myDate.month+8)/5); 
+		//365*year + year/4 - year/100 + year/400 + date + (153*month+8)/5
+		return 1;
+		
 	}
 	
 	public String dayName(DateManager date)
