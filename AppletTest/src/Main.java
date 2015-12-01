5eefde871fee9b4a37184addd7d9c2fdb264d29b
Rob <20183128@U070.college.farnborough>
2015-12-01 16:26:59 +0000

    applet function added

--- AppletTest/src/Main.java
+++ AppletTest/src/Main.java
@@ -4,10 +4,23 @@ import javax.swing.*;
 
 public class Main extends JApplet // errors for some silly eclipse reason
 {
+	private double sum;
+	
+	public void init()
+	{
+		String n1 = JOptionPane.showInputDialog("Enter first number: ");
+		String n2 = JOptionPane.showInputDialog("Enter second number: ");
+		
+		sum = Double.parseDouble(n1) + Double.parseDouble(n2);
+		
+		//String a = JOptionPane.showMessageDialog(sum + " a", sum + " b");
+		
+	}
+	
 	public void paint (Graphics g)
     {
         super.paint(g);
-        g.drawString("wow this worked", 25, 25);
+        g.drawString(sum + "", 25, 25);
     }
 
 }
